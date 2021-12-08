ac176e7bf07a0905ce32b54e7050d6aa7f1fda4e
Bárbara Monzú <monzu.barbara@gmail.com>
2021-12-08 02:09:27 +0100

    Resuelto modelo Menssage

--- client/src/components/services/conversation.service.js
+++ client/src/components/services/conversation.service.js
@@ -7,14 +7,9 @@ class ConversationService {
     })
   }
 
-  // getAllConversations = () => this.app.get("/all")
+  getAllConversations = () => this.app.get("/all")
   createConversation = (idDate) => this.app.post(`/created/${idDate}`)
 
-  getAllMessages = () => this.app.get("/allMessages")
-
-  createMessage = (message, conversation) => this.app.post('/createMessage', (message, conversation))
-  
-  deleteMessage = (idMessage, message, conversation) => this.app.put(`/delete-message/${idMessage}`, { message, conversation })
 }
 
 export default ConversationService
