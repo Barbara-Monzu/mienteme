109d9997295e4c3449bab99fc9c33ee65b30798f
Bárbara Monzú <monzu.barbara@gmail.com>
2021-12-12 13:27:01 +0100

    DatesGastronomy

--- client/src/components/pages/searchCard/SearchCard.js
+++ client/src/components/pages/searchCard/SearchCard.js
@@ -1,36 +1,75 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from "react";
 import './SearchCard.css'
 import HeaderNav from '../headerNav/HeaderNav'
 import FooterNav from '../footerNav/FooterNav'
-// import DatesService from "../../../services/dates.service"
+import { UsersSelected } from "./AllRoutes";
+import DatesService from "../../../services/dates.service"
 
 const SearchCard = (props) => {
     
 const datesService = new DatesService()
-// const dates = []
-// let filteredDate = []
+
+const {allUsers} = useContext(UsersSelected);
+
+// const[loading, setLoading] = useState(false)
+const[allDates, setAllDates] = useState(undefined)
+const[gastronomyDates, setGastronomyDates] = useState(undefined)
+const[filteredDates, setFilteredDates] = useState(undefined)
 
 
-// getDates()
-// filterByDate()
+console.log(" CONTEXTO de todos los usuarios: ", allUsers)
 
-// const getDates = () => {
-//     datesService.getAllDates()
-//       .then(response => {
-//         console.log("estoy mirando todas las citas ==>", response.data)
-//         dates = [...response.data] 
-//       })
-//       .catch(err => console.log("hay un error al conseguir las citas en el SearchBar", err))
-//   }
+    useEffect(() => {
+        getDates()
+        getGastronomyDates()
+        // getCultureDates()
+        // ggetNatureDates()
+        // getRandomDates()
+        // getOthersDates()
+        // getByDay()
+
+    
+      }, [])
 
 
-// const filterByDate = () => {
-//       for(let i = 0; i <= props.users.length; i++) {
-//           let [ one ]= dates.filter( elm => elm.creator === props.users[i]._id )
-//            filteredDate.push(one)
-//       }
+      const getDates = async () => {
+          const response = await datesService.getAllDates()
+              console.log("estoy mirando todas las citas en SEARCH CARD ==>", response.data)
+              setAllDates(response.data)
+              console.log("allDates Despues del SETallDATES", allDates)
+          const filter = await getFilteredDates()   
+        
+        }
 
-//     }
+        const getFilteredDates = () => {
+            let arrDates = []
+              for(let i = 0; i <= allUsers.length; i++) {
+                  let [ dates ] = allDates.filter( elm => elm.creator === allUsers[i]._id )
+                  arrDates.push(dates)
+                  console.log("BUCLE FOR de TODAS LAS CITAS ____> estas son las citas de un user", dates)
+              }
+              setFilteredDates(arrDates);
+        
+            }
+
+            
+      const getGastronomyDates = async () => {
+          const response = await datesService.getGastronomyDates()
+            setGastronomyDates(response.data)
+            let arrDates = []
+            for(let i = 0; i <= allUsers.length; i++) {
+                let [dates]  = gastronomyDates.filter( elm => elm.creator === allUsers[i]._id );
+                arrDates.push(dates)
+                console.log("BUCLE FOR de GASTRONOMY ___>", dates)
+            }
+            setGastronomyDates(arrDates);
+            
+        }
+
+
+
+
+
 
     return (
         <>
