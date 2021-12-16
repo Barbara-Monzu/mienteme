7879143ea098d5d838c2e421a71493b4db6bc2e6
Bárbara Monzú <monzu.barbara@gmail.com>
2021-12-16 11:23:24 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ io.on('connect', socket => {
     })
 
     socket.on('join', ({ username, room }, callback) => {
-        
+
         console.log("QUÉ ES ESTO???", username, room)
 
         const { error, user } = addUser({ id: socket.id, username, room })
@@ -33,32 +33,32 @@ io.on('connect', socket => {
         console.log("ESTE USER--------", user)
         // socket.emit('message', { user: 'admin', text: `${user.username}, welcome to the chat` })
         // socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.username} has joined the chat` })
-    
+
         socket.join(user.room)
-    
+
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
 
-    //DESPUÉS DE TODO ESTO HACE EL setInitialMessages(), que es el callback que le he pasado
+        //DESPUÉS DE TODO ESTO HACE EL setInitialMessages(), que es el callback que le he pasado
         callback()
-      })
+    })
 
 
     socket.on("sendMessage", (message) => {
-        io.emit("receiveMessages", {message} )
+        io.emit("receiveMessages", { message })
 
     })
 
-//   socket.on('sendMessage', (message, callback) => {
-//     const user = getUser(socket.id)
-//     io.to(user.room).emit('message', { user: user.username, message: message })
+    //   socket.on('sendMessage', (message, callback) => {
+    //     const user = getUser(socket.id)
+    //     io.to(user.room).emit('message', { user: user.username, message: message })
 
-//     callback()
-//   })
+    //     callback()
+    //   })
 
-  socket.on('disconnect', () => {
-   io.emit("messages", {server: "Servidor", message: "ha abandonado la sala" })
+    socket.on('disconnect', () => {
+        io.emit("messages", { server: "Servidor", message: "ha abandonado la sala" })
 
-})
+    })
 
 })
 
@@ -71,10 +71,11 @@ io.on('connect', socket => {
 require("./config")(app);
 require("./config/session.config")(app)
 
-
+app.use(express.static(path.join(__dirname, "public")))
 require("./routes")(app)
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 
-require("./error-handling")(app);
+// require("./error-handling")(app);
 
 module.exports = server;
